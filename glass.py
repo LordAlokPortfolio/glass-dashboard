b70618cd87b986d3a8786280a546f92fa7222b52
Alok Kulkarni <clamber.woodcut-0z@icloudcom>
2025-07-02 11:57:34 -0400

    Added pills for years in scratched and prod issue tabs

--- glass.py
+++ glass.py
@@ -141,8 +141,8 @@ with tab2:
         st.dataframe(df_scratch.sort_values(by="Date", ascending=False), use_container_width=True, height=500)
 
     with tab_data2:
-    st.markdown("### 🟥 Production Issue Records")
-    year_filter2 = st.radio("Select Year", sorted(df['Year'].unique(), reverse=True), horizontal=True, key="year2")
-    df_prod = df[(df["Reason"] == "production issue") & (df["Year"] == year_filter2)]
+        st.markdown("### 🟥 Production Issue Records")
+        year_filter2 = st.radio("Select Year", sorted(df['Year'].unique(), reverse=True), horizontal=True, key="year2")
+        df_prod = df[(df["Reason"] == "production issue") & (df["Year"] == year_filter2)]
 
-    st.dataframe(df_prod.sort_values(by="Date", ascending=False), use_container_width=True, height=500)
+        st.dataframe(df_prod.sort_values(by="Date", ascending=False), use_container_width=True, height=500)
